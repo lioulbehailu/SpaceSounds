1233141a3cf1e4352949072a52a52baa2ada8385
SwagBoy2000 <massipietrucci@gmail.com>
2026-06-17 15:20:15 +0200

    All Loops, Effects added
    Added sounds and and updated the fmod effects.
    Edited the planetLoopController to have the third loop option

--- Assets/Scripts/Planet Scripts/PlanetLoopController.cs
+++ Assets/Scripts/Planet Scripts/PlanetLoopController.cs
@@ -6,13 +6,14 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class PlanetLoopController : MonoBehaviour
 {
     private StudioEventEmitter emitter;
-    private int currentState = 0; // 0 = Off, 1 = Loop 1, 2 = Loop 2
+    private int currentState = 0; // 0 = Off, 1 = Loop 1, 2 = Loop 2, 3 = Loop 3
 
     private PlanetVisualFeedback planetVisuals;
 
     [Header("Loop Color Customization")]
     [SerializeField] private Color loop1Color = new Color(0f, 0.6f, 1f, 0.4f); // Transparent Blue
     [SerializeField] private Color loop2Color = new Color(1f, 0.3f, 0f, 0.4f); // Transparent Orange
+    [SerializeField] private Color loop3Color = new Color(0.6f, 0f, 1f, 0.4f); // Transparent Purple
 
     [Header("Rotation Settings")]
     [SerializeField] private float rotationSpeed = 15f;
@@ -32,7 +33,6 @@ public class PlanetLoopController : MonoBehaviour
     void Start()
     {
         emitter = GetComponent<StudioEventEmitter>();
-
         grabInteractable = GetComponent<XRBaseInteractable>();
 
         // Find the PlanetVisuals script sitting on this same root planet object
@@ -70,9 +70,9 @@ public class PlanetLoopController : MonoBehaviour
 
     public void TogglePlanetSound()
     {
-        // Cycle: 0 -> 1 -> 2 -> 0
+        // Cycle: 0 -> 1 -> 2 -> 3 -> 0
         currentState++;
-        if (currentState > 2) currentState = 0;
+        if (currentState > 3) currentState = 0;
 
         if (emitter != null)
         {
@@ -105,6 +105,7 @@ public class PlanetLoopController : MonoBehaviour
     {
         isReleasing = false;
     }
+
     private void ApplyVisualState(int state)
     {
         switch (state)
@@ -130,6 +131,14 @@ public class PlanetLoopController : MonoBehaviour
                 if (planetVisuals != null)
                 {
                     planetVisuals.SetAudioLoopColor(loop2Color); // change color
+                }
+                break;
+
+            case 3: // Loop 3 Active
+                isRotating = true;
+                if (planetVisuals != null)
+                {
+                    planetVisuals.SetAudioLoopColor(loop3Color); // change color
                 }
                 break;
         }
